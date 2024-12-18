9ef67a6de8a8a3ba15c5c8e2acf1209f6f2127a3
renatol1234 <renatoluciano123@hotmail.com>
2024-12-18 14:18:28 -0300

    DDL_Hotel com a tabela contato

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT ||443;
+const port = process.env.PORT ||3000;
 
 const app = express();
 app.set('view engine', 'ejs');
